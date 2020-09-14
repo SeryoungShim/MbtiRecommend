6a12b650da24a4973315dde618fa31b123abeec6
deokhye <qwere2523@gmail.com>
2020-09-14 09:49:46 +0900

    add models

--- MbtiProject/MbtiApp/admin.py
+++ MbtiProject/MbtiApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import DramaInfo, Character
+
+admin.site.register(DramaInfo)
+admin.site.register(Character)
 
 # Register your models here.
--- MbtiProject/MbtiApp/models.py
+++ MbtiProject/MbtiApp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class DramaInfo(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.TextField()
+    plot = models.TextField()
+    site = models.TextField()
+
+class Character(models.Model):
+    drama =  models.ForeignKey(DramaInfo)
+    name = models.CharField(max_length=30)
+    poster = models.TextField()
+    description = models.TextField()
+    personal = models.TextField()
+    mbti = models.CharField(max_length=5)
