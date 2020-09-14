0ecb5bf95e17aeb787f6508efab1c36326a842b2
SeryoungShim <csssr1207@gmail.com>
2020-09-14 18:01:50 +0900

    modify admin page

--- MbtiProject/MbtiApp/views.py
+++ MbtiProject/MbtiApp/views.py
@@ -8,9 +8,7 @@ from .models import DramaInfo, Character
 
 # Create your views here.
 def addDrama(request):
-    context = {
-    "db" : True
-    }
+    context = {}
 
     if request.POST:
         #print(dc.getDrama("넝쿨째 굴러온 당신"))
@@ -18,14 +16,13 @@ def addDrama(request):
         # 1. POST search 값 변수에 저장 - context["title"] 에 저장
         # 2. db 드라마 제목에서 search 값이 있는지 확인
         # 2-1. 있다면 db = True / drama = db읽어오기
-        
         if len(DramaInfo.objects.filter(title = context["title"])) != 0:
             title = DramaInfo.objects.get(title = context["title"])
             context['drama'] = title
-
+            context["db"] = True
         # 2-2. 없다면 db = False
         else:
-            db = False
+            context['db'] = False
         # 3. 아래 pass 지우기
 
     return render(request, "addDrama.html", context)
